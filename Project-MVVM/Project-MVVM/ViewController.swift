1d2bd0a581413740ae64808b5bd477d4af250327
Krishna Gunjal <Kgunjal51@agilitee.com>
2020-11-07 10:43:55 +0530

    initial commit

---
+++ Project-MVVM/Project-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-MVVM
+//
+//  Created by krishna gunjal on 07/11/20.
+//  Copyright © 2020 krishna gunjal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
